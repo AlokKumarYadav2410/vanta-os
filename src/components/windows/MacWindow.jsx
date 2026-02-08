2aa3a81a744e03e287f9deca057a8d8a82f449bc
SubastralAlok <alokkumaryadav241004@gmail.com>
2026-02-08 18:49:19 +0530

    feat: add Spotify component with embedded playlist and update App layout

--- src/components/windows/MacWindow.jsx
+++ src/components/windows/MacWindow.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Rnd } from 'react-rnd'
 import './window.scss'
 
-const MacWindow = ({ children }) => {
+const MacWindow = ({ children, width='40vw', height='50vh' }) => {
     return (
         <Rnd
             default={{
-                width: '40vw',
-                height: '50vh',
+                width: width,
+                height: height,
                 x: 300,
                 y: 20
             }}
